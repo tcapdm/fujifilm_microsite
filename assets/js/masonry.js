b536ca05fe84b0b3c3c1b23ed119ece2af8478d2
unknown <int2170009@OCCPC262.transcosmos-ph.local>
2017-05-11 13:19:10 +0800

    responsive all, color section-click, video autoplay

--- assets/js/masonry.js
+++ assets/js/masonry.js
@@ -2,36 +2,46 @@
 
 $(document).ready(function(){
 	var images = new Array(18);
-
+	var imageH = new Array(18);
+	var ctr=0;
 	for(c1 = 0 ; c1<images.length; c1++){
 		images[c1]="images/designPic"+(c1+1)+".jpg";
+		
+		imageH[c1] = new Image();
+		
+		imageH[c1].onload = function(){
+			ctr+=1;
+			
+			if(ctr  == imageH.length){
+				masonry(images, imageH);
+			}
+		}
+		
+		imageH[c1].src = "images/designPic"+(c1+1)+".jpg";
 	}
 
+});
+
+
+function masonry(images, imageH){
 	var i = 0;
 	while (i < images.length) {
-		
 	
-		$('#masonry-container').append('<div id="img'+(i+1)+'" class="grid-item grid-item--height2"><a href="'+images[i]+'" data-rel="lightcase:myCollection" title="Your title"><img class="zoom" src="'+images[i]+'"  /><span class="bottomRight black-text"><b>&copy; Inagaki Norifumi</b></span></a></div>');
-		
+		$('#masonry-container').append('<div id="img'+(i+1)+'" class="grid-item grid-item--height2"><a href="'+images[i]+'" data-rel="lightcase:myCollection" title="Your title"><img class="zoom" src="'+images[i]+'" /><span class="bottomRight black-text"><b>&copy; Inagaki Norifumi</b></span></a></div>');
+
 		i+=1;
 
 
 	}
-	var masonryCtr =0;
-	var ctrM=0;
+	
 	$('.grid-item--height2').each(function() {
-		masonryCtr +=1;
 //		var h = Math.floor(Math.random() * 100) + 200;
-		var h = images[ctrM].height;
-		console.log(images[ctrM].height);
-		$(this).css("height", h);
-		
+//		var h = imageH[ctrM].height - 220;
+//		console.log(imageH[ctrM].height);
+//		$(this).css("height", h);
 		$('.grid').masonry({
 			itemSelector: '.grid-item'
 		});
-		ctrM+=1;
+	
 	});
-
-
-
-});
+}
